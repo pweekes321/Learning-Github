2c878e69c18ca763a66c7a1224e8cbf3a911dd85
Paul Weekes <paulweekes1@mac.com>
2023-02-27 20:59:10 +0000

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by paul weekes on 27/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
